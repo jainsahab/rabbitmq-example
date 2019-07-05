4524223ed5ed102c8893dc77b9d287b1e6a559fc
Prateek Jain <prateek.jain23995@yahoo.com>
2019-07-05 16:16:00 +0530

    clojure example

--- clojure/src/rabbitmq/tutorials/emit_log.clj
+++ clojure/src/rabbitmq/tutorials/emit_log.clj
@@ -3,17 +3,19 @@
             [langohr.channel :as lch]
             [langohr.exchange :as le]
             [langohr.basic :as lb]
-            [clojure.string :as s]))
+            [clojure.string :as s])
+  (:import (java.util Scanner)))
 
-(def ^{:const true} x "logs")
+(def ^{:const true} exchange-name "clojure-exchange")
 
 (defn -main
   [& args]
   (with-open [conn (lc/connect)]
-    (let [ch      (lch/open conn)
-          payload (if (empty? args)
-                    "Hello, world!"
-                    (s/join " " args))]
-      (le/fanout ch x {:durable false :auto-delete false})
-      (lb/publish ch x "" payload)
-      (println (format " [x] Sent %s" payload)))))
+    (let [ch (lch/open conn)
+          scanner (Scanner. System/in)]
+      (le/fanout ch exchange-name {:durable true :auto-delete false})
+      (while true ((fn []
+                     (println "Enter message")
+                     (let [payload (.nextLine scanner)]
+                       (lb/publish ch exchange-name "" payload)
+                       (println (format " [x] Sent %s" payload)))))))))
--- clojure/src/rabbitmq/tutorials/receive_logs.clj
+++ clojure/src/rabbitmq/tutorials/receive_logs.clj
@@ -6,20 +6,25 @@
             [langohr.basic :as lb]
             [langohr.consumers :as lcons]))
 
-(def ^{:const true} x "logs")
+(def ^{:const true} exchange "clojure-exchange")
+(def ^{:const true} retry-exchange "retry-clojure-exchange")
 
 (defn handle-delivery
   "Handles message delivery"
   [ch metadata payload]
-  (println (format " [x] %s" (String. payload "UTF-8"))))
+  (println (format " [x] %s" (String. payload "UTF-8")))
+  (Thread/sleep 5000)
+  (lb/publish ch retry-exchange "" payload)
+  (lb/ack ch (:delivery-tag metadata) false))
 
 
 (defn -main
   [& args]
-  (with-open [conn (lc/connect)]
+  (with-open [conn (lc/connect {:host "localhost" :port 5672})]
     (let [ch              (lch/open conn)
-          {:keys [queue]} (lq/declare ch "" {:durable true :auto-delete false})]
-      (le/fanout ch x {:durable false :auto-delete false})
-      (lq/bind ch queue x)
+          {:keys [queue]} (lq/declare ch "clojure-queue" {:durable true :auto-delete false})]
+      (le/fanout ch exchange {:durable true})
+      (le/direct ch retry-exchange {:durable true})
+      (lq/bind ch queue exchange)
       (println " [*] Waiting for logs. To exit press CTRL+C")
       (lcons/blocking-subscribe ch queue handle-delivery))))
